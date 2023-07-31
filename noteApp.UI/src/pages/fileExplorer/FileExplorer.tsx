51dc5a557eacc0eb6cbb88236234ac2af620fac1
Matas Razumas <matas.razumas0@gmail.com>
2023-07-31 21:43:38 +0300

    file page ui

--- noteApp.UI/src/pages/fileExplorer/FileExplorer.tsx
+++ noteApp.UI/src/pages/fileExplorer/FileExplorer.tsx
@@ -1,69 +1,93 @@
-import { FC, useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import FolderItem from './components/FolderItem'
-import FileItem from './components/FileItem'
-import DirectoryLabel from './components/DirectoryLabel'
-import FilePage from '../filePage/FilePage'
+import { FC, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import FolderItem from "./components/FolderItem";
+import FileItem from "./components/FileItem";
+import DirectoryLabel from "./components/DirectoryLabel";
+import FilePage from "../filePage/FilePage";
 
-const FileExplorer:FC = () =>
-{
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [currentFolder, setCurrentFolder]:Folder = useState();
-  const [currentFile, setCurrentFile]:File = useState(undefined);
-  //console.log(searchParams.get("path"));
+const FileExplorer: FC = () => {
+  const [searchParams] = useSearchParams();
+  const [currentFolder, setCurrentFolder] = useState<Folder>();
+  const [currentFile, setCurrentFile] = useState<File | undefined>(undefined);
 
   //get from api
-  const data:Folder =
-  {
-    name:"main",
-    folders:[
-      {name:"currentPlan",
-      folders:[
-        {name:'nextCurrentPlan'},
-        {name:"nextCurrentPlan11",
-        folders:[
-          {name:'nextCurrentPlan111'}
-        ]}
-      ]},
-      {name:"currentPlan2",
-      folders:[
-        {name:'nextCurrentPlan2'}
-      ]}
+  const data: Folder = {
+    name: "main",
+    folders: [
+      {
+        name: "currentPlan",
+        folders: [
+          { name: "nextCurrentPlan" },
+          {
+            name: "nextCurrentPlan11",
+            folders: [{ name: "nextCurrentPlan111" }],
+          },
+        ],
+      },
+      { name: "currentPlan2", folders: [{ name: "nextCurrentPlan2" }] },
     ],
-    files:[
-      {name:"txtFile.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile1.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile2.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile3.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile4.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile5.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile6.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
-      {name:"txtFile7.txt",content:"this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng"},
+    files: [
+      {
+        name: "txtFile.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile1.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile2.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile3.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile4.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile5.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile6.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+      {
+        name: "txtFile7.txt",
+        content:
+          "this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng,this is a very long content wow how could i write this on prompt damn i must be good at eberyrtihng",
+      },
+    ],
+  };
 
-    ]
-  }
-
-  useEffect(() =>
-  {
+  useEffect(() => {
     setCurrentFolder(data);
-  }, [])
+  }, []);
 
-  useEffect(() =>
-  {
-    let path = searchParams.get('path');
-    if(!path)
-    {
+  useEffect(() => {
+    let path = searchParams.get("path");
+    if (!path) {
       setCurrentFile(undefined);
       setCurrentFolder(data);
-      return;}
-    let pathArr = path.split('/');
+      return;
+    }
+    let pathArr = path.split("/");
 
     let folderName = pathArr[pathArr.length - 1];
     let isFile = folderName?.split(".").length > 1;
 
-    if(pathArr.length == 1 && isFile)
-    {
-      setCurrentFile(data.files?.find(x => x.name == folderName));
+    if (pathArr.length == 1 && isFile) {
+      setCurrentFile(data.files?.find((x) => x.name == folderName));
       return;
     }
 
@@ -72,59 +96,60 @@ const FileExplorer:FC = () =>
     for (let i = 0; i < pathArr.length; i++) {
       const fn = pathArr[i];
 
-      let temp = cf.folders?.find(x=>x.name == fn);
-      if(!temp) return;
+      let temp = cf.folders?.find((x) => x.name == fn);
+      if (!temp) return;
 
       cf = temp;
     }
 
-    if(isFile)
-      setCurrentFile(cf.files?.find(x => x.name == folderName))
-    else
-      setCurrentFolder(cf);
-
-  }, [searchParams])
-
+    if (isFile) setCurrentFile(cf.files?.find((x) => x.name == folderName));
+    else setCurrentFolder(cf);
+  }, [searchParams]);
 
   //comes here like this files?path="folder1/folder1.1/folder1.1.1"
   //then interate through and get matched last folder name folder item
 
   return (
-    <div className="tw-bg-white tw-pb-3"> { currentFile == undefined ?
-      <>
-        <div className="tw-p-2">
-          <DirectoryLabel label={currentFolder?.name}/>
+    <div className="tw-bg-white tw-pb-3">
+      {currentFile == undefined ? (
+        <>
+          <div className="tw-p-2">
+            <DirectoryLabel label={currentFolder?.name ?? ""} />
           </div>
           <div className="tw-flex tw-flex-col tw-gap-3">
-            {currentFolder?.folders?.map((folder,i) => <FolderItem key={`${folder.name}-${i}`} folder={folder}/>)}
+            {currentFolder?.folders?.map((folder, i) => (
+              <FolderItem key={`${folder.name}-${i}`} folder={folder} />
+            ))}
           </div>
           <div className="tw-mx-2 tw-mt-3 tw-gap-4 tw-grid tw-grid-cols-2 md:tw-grid-cols-4 lg:tw-grid-cols-6">
-            {currentFolder?.files?.map((file,i) => <FileItem key={`${file.name}-${i}`} file={file}/>)}
-            </div>
-      </> : <FilePage file={currentFile} />}
+            {currentFolder?.files?.map((file, i) => (
+              <FileItem key={`${file.name}-${i}`} file={file} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <FilePage file={currentFile} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default FileExplorer;
 
-
 //will always have atleast 1 folder: main
-export interface Folder
-{
-    id?:number
-    name:string;
-    dateCreated?:string;
-    dateModified?:string;
-    folders?:Folder[];
-    files?:File[];
+export interface Folder {
+  id?: number;
+  name: string;
+  dateCreated?: string;
+  dateModified?: string;
+  folders?: Folder[];
+  files?: File[];
 }
 
-export interface File
-{
-  id?:number;
-  dateCreated?:string;
-  dateModified?:string;
-  name:string;
-  content?:string
+export interface File {
+  id?: number;
+  dateCreated?: string;
+  dateModified?: string;
+  name: string;
+  content?: string;
 }
--- noteApp.UI/src/pages/filePage/FilePage.tsx
+++ noteApp.UI/src/pages/filePage/FilePage.tsx
@@ -1,16 +1,41 @@
-import DirectoryLabel from '../fileExplorer/components/DirectoryLabel'
+import { FC, useEffect, useState } from "react";
+import DirectoryLabel from "../fileExplorer/components/DirectoryLabel";
+import { File } from "../fileExplorer/FileExplorer";
+import { toast } from "react-hot-toast";
 
-const FilePage:FC<{ file:File }> = ({ file }) =>
-{
+const FilePage: FC<{ file: File }> = ({ file }) => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    setContent(file.content ?? "");
+  }, [file]);
+
+  const handleSave = () => {
+    toast.success("Saved");
+  };
+
   return (
-    <div>
+    <div className="tw-mx-6 tw-my-2 ">
       <div className="tw-p-2">
-        <DirectoryLabel label={file.name}/>
+        <DirectoryLabel label={file.name} />
       </div>
-      <div>
-        {file.content}
+      <div className="tw-min-h-[200px] tw-border tw-rounded-md tw-bg-">
+        <textarea
+          className="tw-w-full focus:tw-outline-none  tw-min-h-[200px] tw-resize-none"
+          value={content}
+          onChange={(val) => setContent(val.target.value)}
+        />
       </div>
-    </div>)
-}
+      <div className="tw-flex tw-justify-end tw-my-3">
+        <button
+          className="tw-border  tw-py-1.5 tw-px-5 hover:tw-bg-blue-200 tw-bg-blue-100 disabled:tw-bg-blue-50"
+          onClick={handleSave}
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default FilePage;
